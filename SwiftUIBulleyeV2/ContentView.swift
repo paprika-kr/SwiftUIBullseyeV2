a0a0d7586485296b8639bd8b513f165bbd977f5f
Dong-Hyeon Kim <feira@naver.com>
2021-05-06 03:17:58 +0900

    Initial Commit

---
+++ SwiftUIBulleyeV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBulleyeV2
+//
+//  Created by RelMac User Exercise2 on 2021/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBulleyeV2/SwiftUIBulleyeV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBulleyeV2App.swift
+//  SwiftUIBulleyeV2
+//
+//  Created by RelMac User Exercise2 on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBulleyeV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
